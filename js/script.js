980f995b96b1c0375e61dda727dff04621ab5931
seth-obeng <seth.obeng@fjorgedigital.com>
2024-07-12 20:03:47 +0000

    introduce auto play for slider

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
+    autoplay: true,
     prevArrow: '<button type="button" class="slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"></button>',
     customPaging: function(slider, i) {
